92e7f09db205dda2602eba946923556f086f2687
Seulgi <94537395+frankcarreres@users.noreply.github.com>
2024-02-09 17:41:50 +0100

    Alterando la estetica de la fachada

--- ProtoAules/protoAules.py
+++ ProtoAules/protoAules.py
@@ -10,23 +10,22 @@ from recursos_rc import *
 #import firebase_admin
 #from firebase_admin import credentials, firestore
 
-
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
         # Base de datos Firebase
-  #      cred_path = os.path.join(os.path.dirname(
-   #         __file__), "resources/credential.json")
-#
- #       cred = credentials.Certificate(cred_path)
-  #      firebase_admin.initialize_app(cred)
-   #     self.db = firestore.client()
+        #      cred_path = os.path.join(os.path.dirname(
+        #         __file__), "resources/credential.json")
+        #
+        #       cred = credentials.Certificate(cred_path)
+        #      firebase_admin.initialize_app(cred)
+        #     self.db = firestore.client()
 
-        # Inicializar el cargador de UI
+        # Inicialitzar el carregador de UI
         loader = QUiLoader()
 
-        # Cargar pantallas
+        # Cargar pantallas:
         self.login_screen = loader.load(os.path.join(
             os.path.dirname(__file__), "Screens/login_aules.ui"), self)
         self.alumne_ini = loader.load(os.path.join(
@@ -36,36 +35,32 @@ class MainWindow(QtWidgets.QMainWindow):
         self.llistaScreen=loader.load(os.path.join(
             os.path.dirname(__file__), "Screens/llista_alumnes.ui"), self)
 
-        # Añadir pantallas
+        # Afegir pantalles:
         self.stacked_widget = QtWidgets.QStackedWidget(self)
-        # 0
-        self.stacked_widget.addWidget(self.login_screen)
-        # 1
-        self.stacked_widget.addWidget(self.alumne_ini)
-        # 2
-        self.stacked_widget.addWidget(self.profe_ini)
-        # 3
-        self.stacked_widget.addWidget(self.llistaScreen)
+       
+        self.stacked_widget.addWidget(self.login_screen) # 0
+        self.stacked_widget.addWidget(self.alumne_ini) # 1
+        self.stacked_widget.addWidget(self.profe_ini) # 2
+        self.stacked_widget.addWidget(self.llistaScreen) # 3
 
         self.setCentralWidget(self.stacked_widget)
 
-        # Eventos navegación
+        # Events navegació:
         self.login_screen.button_login.clicked.connect(self.accio_login)
-        self.alumne_ini.sortir.clicked.connect(self.show_login)
         self.profe_ini.sortir.clicked.connect(self.show_login)
+        self.profe_ini.llista.clicked.connect(self.show_llista)
+        self.alumne_ini.sortir.clicked.connect(self.show_login)
+        self.llistaScreen.sortir.clicked.connect(self.show_login)        
 
-        #Funcions login
+        #Funcions login:
         self.login_screen.password.returnPressed.connect(self.accio_login)
 
-        #Funcions pantalla alumne
+        #Funcions pantalla alumne:
 
-
-
-
-        #Funcions pantalla profe
+        #Funcions pantalla profe:
         self.profe_ini.llista.clicked.connect(self.show_llista)
         self.profe_ini.ico1.setVisible(False)
-
+        
         menu=QMenu(self)
         act1=QAction("Pujar tramessa",self)
         act1.triggered.connect(self.obrir_fitxer)
@@ -77,39 +72,42 @@ class MainWindow(QtWidgets.QMainWindow):
         self.profe_ini.tool_profesor.setMenu(menu)
         self.profe_ini.tool_profesor2.setMenu(menu)
 
-        #Funcions pantalla llistaAlumnes
-        self.llistaScreen.sortir.clicked.connect(self.show_login)        
-        #self.llistaScreen.cursos.clicked.connect(self.)
+        #Funcions pantalla llistaAlumnes:
+        self.llistaScreen.cursos.clicked.connect(self.show_cursos)
 
-
-
-        # Volver al login
+    # Anar al login
     def show_login(self):
         self.stacked_widget.setCurrentIndex(0)
         self.login_screen.password.clear()
         self.login_screen.username.clear()
         self.login_screen.username.setFocus()
 
+    # Anar a llistaAlumnes
+    def show_llista(self):
+        self.stacked_widget.setCurrentIndex(3)
+
+    # Anar a cursos
+    def show_cursos(self):
+        self.stacked_widget.setCurrentIndex(2)
+
+    #Comprobació d'inici de sessió
     def accio_login(self):
 
-       # Professor
         nombre_user = self.login_screen.username.text() 
        
         pass_user = self.login_screen.password.text() 
        
-
         if not nombre_user[-1].isalpha():
             
             with open('alumnes.json') as json_file:
                     data = json.load(json_file)
             for usuario in data:
                 if usuario["username"] == nombre_user and usuario["password"] == pass_user:    
-                    self.stacked_widget.setCurrentIndex(2)
+                    self.stacked_widget.setCurrentIndex(1)
                     return
-                
+
             msg_error = "El nombre de usuario o contraseña son incorrectos."
             QMessageBox.information(self, "Error al iniciar sesión", msg_error)
-        # Alumne
         else:
             with open('professors.json') as json_file:
                     data2 = json.load(json_file)
@@ -118,31 +116,25 @@ class MainWindow(QtWidgets.QMainWindow):
                     self.stacked_widget.setCurrentIndex(2)
                     return
                     
-                
             msg_error = "El nombre de usuario o contraseña son incorrectos."
             QMessageBox.information(self, "Error al iniciar sesión", msg_error)
-               
-
-    def show_llista(self):
-         self.stacked_widget.setCurrentIndex(3)
 
     def obrir_fitxer(self):
-        #El ,_ es una forma de separar los valores
+        # El ,_ es una forma de separar los valores
         path_doc,_=QFileDialog.getOpenFileName(self, "Seleccionar Archivo","","Archivos de Texto(*.txt);;Archivos PDF(*.pdf)")
         if path_doc:
             doc_nom=os.path.basename(path_doc)
             self.profe_ini.label.setText(doc_nom)
     
-    # def canvi_icona(self):
-    #     archivo = self.label.text().split(": ")[1]
-
-    #     if archivo.endswith(".txt"):
-    #         #Mostrar imagen
-    #     elif archivo.endswith(".pdf"):
-    #         #mostra imatge
-    #     else:
-    #         #mostra imatge global
-
+    # Per a canviar la icona depenent del tipus d'arxiu
+    def canvi_icona(self):
+        archivo = self.label.text().split(": ")[1]
+        if archivo.endswith(".txt"):
+            self.ico1.setText("Hola soc un txt")
+        elif archivo.endswith(".pdf"):
+            self.ico1.setText("Hola soc un pdf")
+        else:
+            self.ico1.setText("Hola soc un random")
 
 def main():
     QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
@@ -150,7 +142,6 @@ def main():
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec())
-
 
 if __name__ == "__main__":
     main()
